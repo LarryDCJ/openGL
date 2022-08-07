ece205cd76b47e9490e673b04a3c43393a39dbec
Larry Donnell Cawley Jr <larrydcj@icloud.com>
2022-08-07 05:09:29 -0400

    coffee cup shaping

--- SceneBuilder.cpp
+++ SceneBuilder.cpp
@@ -17,16 +17,16 @@ void SceneBuilder::UBuildScene(vector<GLMesh>& scene)
          0.0f,  1.0f, 0.0f, 0.0f, // x amount of rotation and rotation @ x y z
          0.0f,  0.0f, 1.0f, 0.0f, // y amount of rotation and rotation @ x y z
          0.0f,  0.0f, 0.0f, 1.0f, // z amount of rotation and rotation @ x y z
-         0.05f, 0.0f, 0.0f,       // translation @ x y z
+         3.0f,  0.0f, 0.0f,       // translation @ x y z
          1.0f,  1.0f              // texture size in relation to the object size
     };
-    coffeeCup.texFilename = "./smiley.png";
+    coffeeCup.texFilename = "textures/coffee_cup.png";
     coffeeCup.height = 0.8f;
     coffeeCup.length = 1.0f;
     coffeeCup.radius = 0.5f;
-    coffeeCup.innerRadius = 0.25f;
-    coffeeCup.numSides = 20;
-    ShapeBuilder::UBuildCylinder(coffeeCup);
+    coffeeCup.innerRadius = 0.475f;
+    coffeeCup.numSides = 360;
+    ShapeBuilder::UBuildHollowCylinder(coffeeCup);
     scene.push_back(coffeeCup);
 
 
