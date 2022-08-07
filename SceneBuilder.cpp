ddc0f036fa47020acf466da3feefad431fd85aef
Larry Donnell Cawley Jr <larrydcj@icloud.com>
2022-08-07 06:22:30 -0400

    done week 5

--- SceneBuilder.cpp
+++ SceneBuilder.cpp
@@ -8,10 +8,10 @@ using namespace std;
 void SceneBuilder::UBuildScene(vector<GLMesh>& scene)
 {
 
-    srand(time(nullptr));
+     srand(time(nullptr));
 
-    GLMesh coffeeCup;
-    coffeeCup.p = {
+     GLMesh coffeeCup;
+     coffeeCup.p = {
          1.0f,  0.0f, 0.0f, 1.0f, // color data rgba
          1.0f,  1.0f, 1.0f,       // scale @ x y z
          0.0f,  1.0f, 0.0f, 0.0f, // x amount of rotation and rotation @ x y z
@@ -19,26 +19,48 @@ void SceneBuilder::UBuildScene(vector<GLMesh>& scene)
          0.0f,  0.0f, 0.0f, 1.0f, // z amount of rotation and rotation @ x y z
          3.0f,  0.0f, 0.0f,       // translation @ x y z
          1.0f,  1.0f              // texture size in relation to the object size
+     };
+     coffeeCup.texFilename = "textures/coffee_cup.png";
+     coffeeCup.height = 0.8f;
+     coffeeCup.length = 1.0f;
+     coffeeCup.radius = 0.5f;
+     coffeeCup.innerRadius = 0.475f;
+     coffeeCup.numSides = 360;
+     ShapeBuilder::UBuildHollowCylinder(coffeeCup);
+     scene.push_back(coffeeCup);
+
+
+
+    GLMesh coffeeCupTop;
+    coffeeCupTop.p = {
+          1.0f,  0.0f, 0.0f, 1.0f, // color data rgba
+          1.0f,  1.0f, 1.0f,       // scale @ x y z
+          0.0f,  1.0f, 0.0f, 0.0f, // x amount of rotation and rotation @ x y z
+          0.0f,  0.0f, 1.0f, 0.0f, // y amount of rotation and rotation @ x y z
+          0.0f,  0.0f, 0.0f, 1.0f, // z amount of rotation and rotation @ x y z
+          3.0f,  0.0f, 0.0f,       // translation @ x y z
+          1.0f,  1.0f              // texture size in relation to the object size
     };
-    coffeeCup.texFilename = "textures/coffee_cup.png";
-    coffeeCup.height = 0.8f;
-    coffeeCup.length = 1.0f;
-    coffeeCup.radius = 0.5f;
-    coffeeCup.innerRadius = 0.475f;
-    coffeeCup.numSides = 360;
-    ShapeBuilder::UBuildHollowCylinder(coffeeCup);
-    scene.push_back(coffeeCup);
+    coffeeCupTop.texFilename = "textures/insideCup.png";
+    coffeeCupTop.height = 0.75f;
+    coffeeCupTop.length = 1.0f;
+    coffeeCupTop.radius = 0.48f;
+    coffeeCupTop.innerRadius = 0.45f;
+    coffeeCupTop.numSides = 360;
+    ShapeBuilder::UBuildCylinder(coffeeCupTop);
+    scene.push_back(coffeeCupTop);
+
 
 
     GLMesh desk;
     desk.p = {
-         1.0f,  1.0f, 1.0f, 1.0f, // color data rgba
-         4.0f,  2.0f, 6.0f,       // scale @ x y z
-         0.0f,  1.0f, 0.0f, 0.0f, // x amount of rotation and rotation @ x y z
-         0.0f,  0.0f, 1.0f, 0.0f, // y amount of rotation and rotation @ x y z
-         0.0f,  0.0f, 0.0f, 1.0f, // z amount of rotation and rotation @ x y z
-         1.0f,  0.0f, 0.0f,       // translation @ x y z
-         1.0f,  1.0f              // texture size in relation to the object size
+        1.0f,  1.0f, 1.0f, 1.0f, // color data rgba
+        4.0f,  2.0f, 6.0f,       // scale @ x y z
+        0.0f,  1.0f, 0.0f, 0.0f, // x amount of rotation and rotation @ x y z
+        0.0f,  0.0f, 1.0f, 0.0f, // y amount of rotation and rotation @ x y z
+        0.0f,  0.0f, 0.0f, 1.0f, // z amount of rotation and rotation @ x y z
+        1.0f,  0.0f, 0.0f,       // translation @ x y z
+        1.0f,  1.0f              // texture size in relation to the object size
     };
     desk.texFilename = "wood.png";
     ShapeBuilder::UBuildPlane(desk);
